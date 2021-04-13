cb92ba3a5b0acbe12dd049f064fb7353787eab87
WillianFarias <williansousafarias@gmail.com>
2021-04-12 23:11:44 -0300

    aplicando conceitos de estado e imutabilidade, useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+//useState permite aplicar o conceito de estado a variaveis 
+
+import Header from './components/Header';
 
 function App() {
-  return <h1>Hello Word</h1>;
+
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`);
+    //aplicando imutabilidade
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+  return (
+    //fragment
+    //O react exige que o elemento principal tenha um key
+    <>
+      <Header title = "Projects" />
+      {projects.map(project => <li key={project}>{ project }</li>)}
+
+      <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+    </>
+  )
 }
 
 export default App;
