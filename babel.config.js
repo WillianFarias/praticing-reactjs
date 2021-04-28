3e06849b92f853847cdbb75c1cfd18138912f06b
WillianFarias <williansousafarias@gmail.com>
2021-04-28 20:44:53 -0300

    reiniciando projeto

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    //permite escrever html dentro do js
-    '@babel/preset-react'
-  ],
-};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './App';
-
-//isso nao é um componente. JSX (JS XML) HTML dentro do JS
-render(<App />, document.getElementById('app'));
