afe201832d0ada358a31cc2392abb863bdded3ea
WillianFarias <williansousafarias@gmail.com>
2021-04-30 20:18:47 -0300

    aplicando css e config de script de execucao d aaplicacao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+
+import './App.css';
 import Header from './components/Header';
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
   function handleAddProject(){
     //projects.push(`Novo projeto ${Date.now()}`);
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
-    
+
     console.log(projects);
   }
 
