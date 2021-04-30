6430d4c888d1ea5412217eba0e4bd10eb963f84b
WillianFarias <williansousafarias@gmail.com>
2021-04-30 17:50:31 -0300

    praticando conceito de propriedade(children) e componentizacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <Header title="No">
+        <ul>
+          <li>Login</li>
+          <li>Tênis</li>
+          <li>Yeezy</li>
+        </ul>
+      </Header>
+
+      <Header title="Word!">
+      <ul>
+          <li>Login</li>
+          <li>Jordan</li>
+          <li>Yeezy</li>
+        </ul>
+      </Header>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './App';
+import Header from './components/Header';
 
+//JSX
 render(<App />, document.getElementById('app'));
